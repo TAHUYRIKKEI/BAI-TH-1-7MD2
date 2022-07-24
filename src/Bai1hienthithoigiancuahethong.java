5af7c8373b04e48918b5a988497e40baa9a3d9eb
unknown <tavanhuyvp8590@gmail.com>
2022-07-25 04:57:23 +0700

    BAI THUC HANH

---
+++ src/Bai1hienthithoigiancuahethong.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Bai1hienthithoigiancuahethong {
+    public static void main(String[]args){
+        Date date = new Date();
+        System .out.println("Now is"+date);
+    }
+
+}
